80de2a760cb41e207f722ab5c0bd353b9230b2b5
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-12 19:20:13 +0200

    Update menfcall.py

--- AnonX/plugins/menfcall.py
+++ AnonX/plugins/menfcall.py
@@ -7,45 +7,29 @@ from AnonX.core.call import Anon
 from AnonX.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
 
-
 @app.on_message(filters.regex("^مين في الكول$"))
 async def strcall(client, message):
-    assistant = await group_assistant(Anon,message.chat.id)
+    assistant = await group_assistant(Yukki,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./AnonX/assets/call.mp3"), stream_type=StreamType().pulse_stream)
-        text="الناس الموجودين ف الكول:\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/vega.mp3"), stream_type=StreamType().pulse_stream)
+        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث "
+                mut="يتحدث 🗣"
             else:
-                mut="ساكت "
+                mut="ساكت 🔕"
             user = await client.get_users(participant.user_id)
             k +=1
             text +=f"{k}➤{user.mention}➤{mut}\n"
         text += f"\nعددهم : {len(participants)}\n✔️"    
         await message.reply(f"{text}")
-        await asyncio.sleep(7)
+        await asyncio.sleep(5)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"الكول مقفول اصلا يصاحبي")
+        await message.reply(f"عمووووو الكول مش مفتوح اصلااا\n❌")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام")
-    except AlreadyJoinedError:
-        text="الناس الموجودين ف الكول:\n\n"
-        participants = await assistant.get_participants(message.chat.id)
-        k =0
-        for participant in participants:
-            info = participant
-            if info.muted == False:
-                mut="يتحدث "
-            else:
-                mut="ساكت "
-            user = await client.get_users(participant.user_id)
-            k +=1
-            text +=f"{k}➤{user.mention}➤{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
-        await message.reply(f"{text}")
+        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
         
