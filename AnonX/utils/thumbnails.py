930e62b1d80fdff8f2c60ba2659dae31c530898d
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-17 14:37:00 +0200

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -91,7 +91,7 @@ async def gen_thumb(videoid, user_id):
 
         image3 = changeImageSize(1280, 720, bg)
         
-            circle = Image.open(f"assets/almortagel.png")
+            circle = Image.open(f"AnonX/assets/anonx.png")
 
             # changing circle color
             im = circle
